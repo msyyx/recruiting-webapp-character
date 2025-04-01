b0abe4e04df0b275a6f9f0d55785c0bf55198374
Sam <17864601+msyyx@users.noreply.github.com>
2025-04-01 01:51:59 -0700

    completed requirement 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
 import { Class } from './class';
+import { Skill } from './skill';
 
 function App() {
   const [attributes, setAttributes] = useState({
@@ -30,6 +31,8 @@ function App() {
       modifier: -1,
     },
   });
+  const [charSkills, setSkills] = useState({});
+  const [availableSkillPoints, setAvailableSkillPoints] = useState(10 + 4 * attributes.Intelligence.modifier)
 
   const modifyAttribute = (attribute, val) => {
     const updatedAttributes = {
@@ -43,6 +46,23 @@ function App() {
       (updatedAttributes[attribute].value - 10) / 2
     );
     setAttributes(updatedAttributes);
+  };
+
+  const modifySkillPoints = (skill, val) => {
+    if (availableSkillPoints - val < 0) return
+
+    const updatedSkills = {
+      ...charSkills,
+    };
+    if (!(skill.name in updatedSkills))
+      updatedSkills[skill.name] = {
+        points: 0,
+        total: attributes[skill.attributeModifier].modifier,
+      };
+    updatedSkills[skill.name].points = updatedSkills[skill.name].points + val;
+    updatedSkills[skill.name].total = updatedSkills[skill.name].total + val;
+    setAvailableSkillPoints(availableSkillPoints - val)
+    setSkills(updatedSkills);
   };
 
   return (
@@ -74,6 +94,24 @@ function App() {
               className={charClass[0]}
               classRequirements={charClass[1]}
               charAttributes={attributes}
+            />
+          ))}
+        </div>
+        <div>
+          <h3>Skills</h3>
+          <p>Skill Points Available: {availableSkillPoints}</p>
+          {SKILL_LIST.map(skill => (
+            <Skill
+              key={skill.name}
+              skill={skill}
+              charParams={{
+                points: charSkills[skill.name]?.points || 0,
+                modifierValue: attributes[skill.attributeModifier].modifier,
+                total: charSkills[skill.name]?.total != null
+                  ? charSkills[skill.name].total
+                  : attributes[skill.attributeModifier].modifier
+              }}
+              modifySkillPoints={modifySkillPoints}
             />
           ))}
         </div>
---
+++ src/skill.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
+
+export function Skill({ skill, charParams, modifySkillPoints }) {
+  return (
+    <div>
+      {skill.name} - points: {charParams.points}
+      <button onClick={() => modifySkillPoints(skill, 1)}>+</button>
+      <button onClick={() => modifySkillPoints(skill, -1)}>-</button>
+      modifier {skill.modifier}: {charParams.modifierValue} total: {charParams.total}
+    </div>
+  );
+}
