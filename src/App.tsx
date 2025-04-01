84feb77a2d5665961ed13eed5a798326db24ff4b
Sam <17864601+msyyx@users.noreply.github.com>
2025-04-01 01:18:18 -0700

    completed requirement 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,35 @@ import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
 
 
 function App() {
-  const [num, setNum] = useState<number>(0);
+  const [attributes, setAttributes] = useState({
+    Strength: {
+      value: 9,
+    },
+    Dexterity: {
+      value: 9,
+    },
+    Constitution: {
+      value: 9,
+    },
+    Intelligence: {
+      value: 9,
+    },
+    Wisdom: {
+      value: 9,
+    },
+    Charisma: {
+      value: 9,
+    },
+  })
+
+  const modifyAttribute = (attribute, val) => {
+    const updatedAttributes = {
+      ...attributes,
+    }
+    updatedAttributes[attribute].value = Math.max(attributes[attribute].value + val, 0)
+    setAttributes(updatedAttributes)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,14 +40,22 @@ function App() {
       </header>
       <section className="App-section">
         <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
+          <h3>Attributes</h3>
+          {Object.entries(attributes).map(attribute => (
+            <div key={attribute[0]}>
+              {attribute[0]}:{attribute[1].value}
+              <button onClick={() => modifyAttribute(attribute[0], 1)}>
+                +
+              </button>
+              <button onClick={() => modifyAttribute(attribute[0], -1)}>
+                -
+              </button>
+            </div>
+          ))}
         </div>
       </section>
     </div>
-  );
+  )
 }
 
 export default App;
